8661e3b0b4f10a41048b8c698870978f08715543
roy kimathi <kimathiwaweru@gmail.com>
2023-07-28 05:55:04 +0300

    feature: auth components - create auth folder, add signup login profile components

--- src/App.js
+++ src/App.js
@@ -1,39 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
-import React, { useState, useEffect } from "react";
-import { Routes, Route } from 'react-router-dom';
-
-import NavBar from './Components/NavBar/NavBar.js';
-import Home from './Components/Home/Home.js';
-import Login from './Components/Login/Login.js';
-import DoctorProfile from './Components/DoctorProfile/DoctorProfile';
-import Portal from './Components/Portal/Portal.js';
-import PortalPatients from './Components/PortalPatients/PortalPatients.js'
-import PortalAppts from './Components/PortalAppts/PortalAppts.js';
-import PortalLabResults from './Components/PortalLabResults/PortalLabResults.js';
-import PortalCalendar from './Components/PortalCalendar/PortalCalendar.js'
-
+import React from "react";
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
+import Signup from "./Components/auth/Signup.js"
+import Login from "./Components/auth/Login.js"
+import Profile from "./Components/auth/Profile.js"
+import LandingPage from './Components/LandingPage.js'
+import NavBar from "./Components/NavBar.js";
 
 function App() {
 
+  return (
+    <>
+    <Router>
+      <NavBar />
+      <Routes>
+      <Route path="/signup" element={<Signup />}/>
+      <Route path="/login" element={<Login />}/>
+      <Route path="/profile" element={<Profile />}/>
+
+      <Route path="/" element={<LandingPage />} />
+      </Routes>
+      
+    </Router>
+    </>
+  );
 }
-
-return (
-  <div className="App">
-    <NavBar user={user} setUser={setUser} />
-    <Routes>
-      <Route exact path = '/' element={<Home/>}/>
-      <Route path = '/login' element={<Login setUser={setUser} />}/>
-      <Route path = '/doctors/:id' element={<DoctorProfile doc={doc} />}/>
-      <Route path = '/portal' element={<Portal user={user} />}/>
-      <Route path = '/portal/patients' />
-      <Route path = '/portal/calendar' element={<PortalCalendar docAppointments={docAppointments} user={user} patientAppts={patientAppts} patients={patients} patientNames={patientNames} />}/>
-      <Route path='/portal/appointments' element={<PortalAppts patientAppts={patientAppts} user={user} /> } />
-      <Route path='/portal/labresults' element={<PortalLabResults user={user} results={results} /> } />
-    </Routes>
-    <Footer />
-  </div>
-);
-
 
 export default App;
