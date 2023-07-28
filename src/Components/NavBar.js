c3bce187aae304b20a23f0754854074a2f46c6a8
roy kimathi <kimathiwaweru@gmail.com>
2023-07-28 12:55:30 +0300

    feature: signup-button-add signup button to landingpage

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,7 +1,17 @@
 import React from 'react'
+import Container from "react-bootstrap/Container"
+import Navbar from'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav'
 
 export default function NavBar() {
   return (
-    <div>NavBar</div>
+    <Navbar expand='lg' className='bg-body-tertiary'>
+      <Container>
+        <Navbar.Brand href='/' style={{fontFamily:"monospace", fontWeight:"bolder"}}>
+          <img style={{height:"40px", width:"50px"}} src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTufoR1gl8noO1RA2PcSjOi2F133VMQGB7IIw&usqp=CAU'/>
+          Linda Mama
+          </Navbar.Brand>
+      </Container>
+    </Navbar>
   )
 }
