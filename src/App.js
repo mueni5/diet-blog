358fa76f00fd5b16dd53def99b62752c85468e7c
+ <+>
2023-07-28 13:45:17 +0300

    feature:routes -update routes to include chat forum component

--- src/App.js
+++ src/App.js
@@ -1,39 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
-import React, { useState, useEffect } from "react";
-import { Routes, Route } from 'react-router-dom';
+import './App.css'
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-import NavBar from './Components/NavBar/NavBar.js';
-import Home from './Components/Home/Home.js';
-import Login from './Components/Login/Login.js';
-import DoctorProfile from './Components/DoctorProfile/DoctorProfile';
-import Portal from './Components/Portal/Portal.js';
-import PortalPatients from './Components/PortalPatients/PortalPatients.js'
-import PortalAppts from './Components/PortalAppts/PortalAppts.js';
-import PortalLabResults from './Components/PortalLabResults/PortalLabResults.js';
-import PortalCalendar from './Components/PortalCalendar/PortalCalendar.js'
+import ChatForum from './components/ChatForum/ChatForum';
 
-
-function App() {
-
-}
-
-return (
-  <div className="App">
-    <NavBar user={user} setUser={setUser} />
-    <Routes>
-      <Route exact path = '/' element={<Home/>}/>
-      <Route path = '/login' element={<Login setUser={setUser} />}/>
-      <Route path = '/doctors/:id' element={<DoctorProfile doc={doc} />}/>
-      <Route path = '/portal' element={<Portal user={user} />}/>
-      <Route path = '/portal/patients' />
-      <Route path = '/portal/calendar' element={<PortalCalendar docAppointments={docAppointments} user={user} patientAppts={patientAppts} patients={patients} patientNames={patientNames} />}/>
-      <Route path='/portal/appointments' element={<PortalAppts patientAppts={patientAppts} user={user} /> } />
-      <Route path='/portal/labresults' element={<PortalLabResults user={user} results={results} /> } />
-    </Routes>
-    <Footer />
-  </div>
-);
-
+const App = () => {
+  return (
+    <Router>
+      <div>
+        <Routes>
+          
+          <Route path="/Chat-forum" element={<ChatForum />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+};
 
 export default App;
