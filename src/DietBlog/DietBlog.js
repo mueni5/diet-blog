2112155d81befd4402a529ebc96f8bf017cfdaef
brigitteorina <brigitteorina@gmail.com>
2023-07-31 22:08:39 +0300

    feature:diet_blog - change blog object

--- src/DietBlog/DietBlog.js
+++ src/DietBlog/DietBlog.js
@@ -1,7 +1,4 @@
-
 import './DietBlog.css';
-
-
 function Card({image, title}){
     return(
         <div className="card">
@@ -18,115 +15,39 @@ function Card({image, title}){
 
 export default function DietBlog() {
     const blogPosts = [
-    {
-      "id": 101,
-      "name": "wHz-93",
-      "health": 94,
-      "damage": 20,
-      "armor": 63,
-      "bot_class": "Support",
-      "catchphrase": "1010010101001101100011000111101",
-      "avatar_url": "https://robohash.org/nostrumrepellendustenetur.png?size=300x300&set=set1",
-      "created_at": "2018-10-02T19:55:10.800Z",
-      "updated_at": "2018-10-02T19:55:10.800Z"
-    },
-    {
-      "id": 102,
-      "name": "RyM-66",
-      "health": 86,
-      "damage": 36,
-      "armor": 77,
-      "bot_class": "Medic",
-      "catchphrase": "0110011100000100011110100110011000011001",
-      "avatar_url": "https://robohash.org/quidemconsequaturaut.png?size=300x300&set=set1",
-      "created_at": "2018-10-02T19:55:10.827Z",
-      "updated_at": "2018-10-02T19:55:10.827Z"
-    },
-    {
-      "id": 103,
-      "name": "P-24",
-      "health": 88,
-      "damage": 25,
-      "armor": 60,
-      "bot_class": "Witch",
-      "catchphrase": "1100101001110111110010011010110",
-      "avatar_url": "https://robohash.org/idetcumque.png?size=300x300&set=set1",
-      "created_at": "2018-10-02T19:55:10.844Z",
-      "updated_at": "2018-10-02T19:55:10.844Z"
-    },
-    {
-      "id": 104,
-      "name": "qq-69",
-      "health": 74,
-      "damage": 29,
-      "armor": 95,
-      "bot_class": "Defender",
-      "catchphrase": "00011101001001000000101100001100110",
-      "avatar_url": "https://robohash.org/corruptieaqueexcepturi.png?size=300x300&set=set1",
-      "created_at": "2018-10-02T19:55:10.856Z",
-      "updated_at": "2018-10-02T19:55:10.856Z"
-    },
-    {
-      "id": 105,
-      "name": "G`t-65",
-      "health": 62,
-      "damage": 98,
-      "armor": 31,
-      "bot_class": "Assault",
-      "catchphrase": "010101111110111111101101010110010100101",
-      "avatar_url": "https://robohash.org/nisinequequas.png?size=300x300&set=set1",
-      "created_at": "2018-10-02T19:55:10.867Z",
-      "updated_at": "2018-10-02T19:55:10.867Z"
-    },
-    {
-      "id": 106,
-      "name": "TI-09",
-      "health": 81,
-      "damage": 39,
-      "armor": 52,
-      "bot_class": "Support",
-      "catchphrase": "00111100111001011111110010110111",
-      "avatar_url": "https://robohash.org/etutprovident.png?size=300x300&set=set1",
-      "created_at": "2018-10-02T19:55:10.887Z",
-      "updated_at": "2018-10-02T19:55:10.887Z"
-    },
-    {
-      "id": 107,
-      "name": "XSf-63",
-      "health": 97,
-      "damage": 26,
-      "armor": 58,
-      "bot_class": "Support",
-      "catchphrase": "0010110001011100111100101010101000",
-      "avatar_url": "https://robohash.org/accusantiumetfacilis.png?size=300x300&set=set1",
-      "created_at": "2018-10-02T19:55:10.899Z",
-      "updated_at": "2018-10-02T19:55:10.899Z"
-    },
-    {
-      "id": 108,
-      "name": "m-76",
-      "health": 65,
-      "damage": 90,
-      "armor": 38,
-      "bot_class": "Assault",
-      "catchphrase": "1101111100001011111011011001011110010000",
-      "avatar_url": "https://robohash.org/blanditiisautlaudantium.png?size=300x300&set=set1",
-      "created_at": "2018-10-02T19:55:10.915Z",
-      "updated_at": "2018-10-02T19:55:10.915Z"
-    },
-    {
-      "id": 109,
-      "name": "hjk-40",
-      "health": 75,
-      "damage": 84,
-      "armor": 31,
-      "bot_class": "Witch",
-      "catchphrase": "11010100110000100001110001100000010111101000001100",
-      "avatar_url": "https://robohash.org/quirationerem.png?size=300x300&set=set1",
-      "created_at": "2018-10-02T19:55:10.940Z",
-      "updated_at": "2018-10-02T19:55:10.940Z"
-    }
-]
+   {
+      "id":1,
+      "title":"Diet Blog 1",
+      "image":"https://images.unsplash.com/photo-1611095777879-9e9a3e0a5b7a?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8ZGlldCUyMGJsb2d8ZW58MHx8MHx8\u0026ixlib=rb-1.2.1\u0026w=1000\u0026q=80",
+      "content":"This is the first diet blog",
+      "created_at":"2023-07-31T18:52:17.322Z",
+      "updated_at":"2023-07-31T18:52:17.322Z"
+   },
+   {
+      "id":2,
+      "title":"Diet Blog 2",
+      "image":"https://images.unsplash.com/photo-1611095777879-9e9a3e0a5b7a?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8ZGlldCUyMGJsb2d8ZW58MHx8MHx8\u0026ixlib=rb-1.2.1\u0026w=1000\u0026q=80",
+      "content":"This is the second diet blog",
+      "created_at":"2023-07-31T18:52:17.329Z",
+      "updated_at":"2023-07-31T18:52:17.329Z"
+   },
+   {
+      "id":3,
+      "title":"Diet Blog 3",
+      "image":"https://images.unsplash.com/photo-1611095777879-9e9a3e0a5b7a?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8ZGlldCUyMGJsb2d8ZW58MHx8MHx8\u0026ixlib=rb-1.2.1\u0026w=1000\u0026q=80",
+      "content":"This is the third diet blog",
+      "created_at":"2023-07-31T18:52:17.335Z",
+      "updated_at":"2023-07-31T18:52:17.335Z"
+   },
+   {
+      "id":4,
+      "title":"Diet Blog 4",
+      "image":"https://images.unsplash.com/photo-1611095777879-9e9a3e0a5b7a?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8ZGlldCUyMGJsb2d8ZW58MHx8MHx8\u0026ixlib=rb-1.2.1\u0026w=1000\u0026q=80",
+      "content":"This is the fourth diet blog",
+      "created_at":"2023-07-31T18:52:17.341Z",
+      "updated_at":"2023-07-31T18:52:17.341Z"
+   }
+    ]
 
 
     return (
@@ -134,7 +55,7 @@ export default function DietBlog() {
             <h1>Diet Blog</h1>
             <div className="card-container">
             {blogPosts.map((post, index) => (
-            <Card key={index} image={post.avatar_url} title={post.avatar_url} />
+            <Card key={index} image={post.image} title={post.title} />
         ))}
         </div>
         </div>
